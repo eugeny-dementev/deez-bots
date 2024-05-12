9a4fedd34b9b91eddab40d027d6dca9269f75bad
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-05-12 14:46:30 +0800

    test: cover prepare and exec with unit tests

--- libs/command/src/lib/command.spec.ts
+++ libs/command/src/lib/command.spec.ts
@@ -1,7 +1,21 @@
-import { command } from './command';
+import { prepare, exec } from './command';
 
-describe('command', () => {
-  it('should work', () => {
-    expect(command()).toEqual('command');
+describe('libs/command', () => {
+  describe('prepare', () => {
+    it('should prepare commoand line for yt-dlp', () => {
+      expect(prepare('yt-dlp').toString()).toEqual('yt-dlp');
+    });
+
+    it('should prepare command line for yt-dlp with args', () => {
+      expect(prepare('yt-dlp').add('-S "res:720"').toString()).toEqual('yt-dlp -S "res:720"');
+    });
   });
+
+  describe('exec', () => {
+    it('should execute commoand and return stdout', async () => {
+      const stdout = await exec('echo hello');
+
+      expect(stdout.trim()).toEqual('hello');
+    });
+  })
 });
