76110f46baab857eb95d2f9a176cd56fc5e80fc2
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-05-12 14:46:17 +0800

    feat: create prepare and exec method of command lib

--- libs/command/src/lib/command.ts
+++ libs/command/src/lib/command.ts
@@ -1,3 +1,49 @@
-export function command(): string {
-  return 'command';
+import shelljs from 'shelljs';
+
+class CommandLine {
+  readonly #command: string;
+  readonly #args: string[] = [];
+
+  constructor(command: string) {
+    this.#command = command;
+  }
+
+  add(parameter: string, add: boolean = true) {
+    if (add) {
+      this.#args.push(parameter);
+    }
+
+    return this;
+  }
+
+  toString() {
+    return [
+      this.#command,
+      ...this.#args,
+    ].join(' ');
+  }
+}
+
+export function prepare(command: string): CommandLine {
+  return new CommandLine(command);
+}
+
+export async function exec(commandLine: string): Promise<string> {
+  return new Promise((res, rej) => {
+    if (!commandLine) {
+      rej(new TypeError('Command not passed'));
+
+      return;
+    }
+
+    shelljs.exec(commandLine!, { async: true }, (code, stdout, stderr) => {
+      if (code === 0) {
+        res(stdout.toString());
+
+        return;
+      }
+
+      rej(stderr.toString());
+    });
+  });
 }
