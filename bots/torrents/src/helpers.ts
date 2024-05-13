ac30289f482e5707e177e7a9ba78b05e4d09defb
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-05-13 22:38:59 +0800

    feat: make open and closing browser helpers

--- bots/torrents/src/helpers.ts
+++ bots/torrents/src/helpers.ts
@@ -1,4 +1,7 @@
-export function nameCheckerFactory (name: string) {
+import path from "path";
+import { BrowserType, BrowserContext, Page } from "playwright";
+
+export function nameCheckerFactory(name: string) {
   const re = new RegExp(name, 'i');
   return (item: string) => re.test(item);
 }
@@ -26,4 +29,37 @@ export function omit(obj: object, ...keys: string[]) {
 
 export function sleep(timeout: number) {
   return new Promise((res) => setTimeout(res, timeout));
+}
+
+export function getUserDataPath(): string {
+  return path.resolve(process.cwd(), 'userData');
+}
+
+export async function openBrowser(chromium: BrowserType<{}>): Promise<{ browser: BrowserContext; page: Page; }> {
+  const browser = await chromium.launchPersistentContext(getUserDataPath(), { headless: true });
+  const pages = browser.pages()
+
+  for (const page of pages) {
+    page.close();
+  }
+
+  const page: Page = await browser.newPage();
+
+  const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36';
+  await page.setExtraHTTPHeaders({
+    'User-Agent': userAgent,
+    'Accept-Language': 'en-US,en;q=0.9'
+  });
+
+  return { browser, page };
+}
+
+export async function closeBrowser(browser: BrowserContext): Promise<void> {
+  const pages = browser.pages();
+
+  for (const page of pages) {
+    page.close();
+  }
+
+  await browser.close();
 }
