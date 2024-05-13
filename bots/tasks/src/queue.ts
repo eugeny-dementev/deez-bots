441d3252dbcfc4f416ff4aaee692669855d61a3a
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-05-13 22:00:03 +0800

    mvp: prepare possible queue structur for bots/tasks

---
+++ bots/tasks/src/queue.ts
@@ -0,0 +1,17 @@
+export const addTask = () => [
+  /*
+  CheckFileExists,
+  ContainURL, {
+    true: [
+      notification.tlog('Extracting metadata'),
+      ScrapeMicrodata,
+      FormatMetadataToMd,
+    ],
+    false: [
+      FormatTextToMd,
+    ],
+  },
+  WriteMdToFile,
+  notification.tlog('Task added'),
+  */
+];
