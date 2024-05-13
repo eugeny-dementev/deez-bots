b21538cfaba3e9ca76bde20c1af7f27e44c8a93e
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-05-13 22:22:03 +0800

    feat: compact bot.launch callback

--- bots/torrents/src/main.ts
+++ bots/torrents/src/main.ts
@@ -101,9 +101,7 @@ bot.use((ctx) => {
   }
 })
 
-bot.launch(() => {
-  console.log('Bot launched');
-});
+bot.launch(() => console.log('Bot launched'));
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
