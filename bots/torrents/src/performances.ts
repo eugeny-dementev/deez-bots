02ca29064d3251a9b4c32a793e025b9d8abff3ce
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-05-19 13:24:22 +0800

    feat: update torrents queue

--- bots/torrents/src/performances.ts
+++ bots/torrents/src/performances.ts
@@ -1,19 +1,36 @@
+import { InjectLogger, InjectNotifications, notifications } from '@libs/actions';
 import { QueueAction, util } from 'async-queue-runner';
 import {
   AddUploadToQBitTorrent,
   CheckTorrentFile,
+  ConvertMultiTrack,
   DeleteFile,
+  ExtractTorrentPattern,
   Log,
   MonitorDownloadingProgress,
-  ExtractTorrentPattern
 } from './actions.js';
+import { MultiTrackContext } from './types.js';
 
 export const handleQBTFile: () => QueueAction[] = () => [
+  InjectLogger,
+  InjectNotifications,
   Log,
   ExtractTorrentPattern,
   CheckTorrentFile,
+  util.if<MultiTrackContext>(({ type }) => type === 'multi-track', {
+    then: [
+      notifications.tlog('Multi Track torrent detected'),
+    ],
+  }),
+  Log,
   AddUploadToQBitTorrent,
   util.delay(5000),
   MonitorDownloadingProgress,
+  util.if<MultiTrackContext>(({ type }) => type === 'multi-track', {
+    then: [
+      notifications.tlog('Start multiplexing'),
+      ConvertMultiTrack,
+    ],
+  }),
   DeleteFile,
 ];
