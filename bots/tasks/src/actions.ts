6fd751e1cba1f3de495b220536abe60ded449a2c
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-05-25 23:25:11 +0800

    feat: add extra stealth mode to playwright for tasks bot

--- bots/tasks/src/actions.ts
+++ bots/tasks/src/actions.ts
@@ -1,6 +1,18 @@
 import { Action, QueueContext } from 'async-queue-runner'
-import { chromium } from 'playwright';
+// import { chromium } from 'playwright';
+// playwright-extra is a drop-in replacement for playwright,
+// it augments the installed playwright with plugin functionality
+import { chromium } from 'playwright-extra';
 import { closeBrowser, openBrowser } from './helpers';
+import { LoggerOutput, NotificationsOutput } from '@libs/actions';
+
+
+// Load the stealth plugin and use defaults (all tricks to hide playwright usage)
+// Note: playwright-extra is compatible with most puppeteer-extra plugins
+const stealth = require('puppeteer-extra-plugin-stealth')()
+
+// Add the plugin to playwright (any number of plugins can be added)
+chromium.use(stealth)
 
 const metascraper = require('metascraper')([
   require('metascraper-author')(),
--- bots/tasks/src/helpers.ts
+++ bots/tasks/src/helpers.ts
@@ -3,6 +3,10 @@ import { Browser, BrowserType, Page } from "playwright";
 export async function openBrowser(chromium: BrowserType<{}>): Promise<{ browser: Browser; page: Page; }> {
   const browser = await chromium.launch();
 
+  const context = await browser.newContext();
+
+  await context.addInitScript("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+
   const page: Page = await browser.newPage();
 
   const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36';
