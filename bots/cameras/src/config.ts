b676dd0dfc021551d5f00ad32e4f31766a27f2eb
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-06-01 21:41:09 +0800

    feat: add cameraCorridorUrl config

--- bots/cameras/src/config.ts
+++ bots/cameras/src/config.ts
@@ -1,5 +1,5 @@
 export const swapDir = process.env.SWAP_DIR;
-export const homeDir = process.env.HOME_DIR;
+export const homeDir = process.env.HOME_DIR as string;
 
 if (!homeDir || !swapDir) throw new Error('SWAP_DIR and HOME_DIR must be specified');
 
@@ -10,3 +10,9 @@ export const publishersIds = String(process.env.PUBLISHERS_IDS)
   .map((id: string): number => parseInt(id))
 
 export const adminId = Number(process.env.ADMIN_ID);
+
+export const cameraCorridorUrl = process.env.CAMERA_CORRIDOR as string;
+
+if (!cameraCorridorUrl) {
+  throw new Error('CAMERA_CORRIDOR must be provided');
+}
