2cc1c0e7133e9ffec7ef1d34b4a053d65a2b36fd
Eugeny Dementev <eugeny.dementev@ya.ru>
2024-06-01 16:11:21 +0800

    feat: bots/cameras

---
+++ bots/cameras/src/config.ts
@@ -0,0 +1,12 @@
+export const swapDir = process.env.SWAP_DIR;
+export const homeDir = process.env.HOME_DIR;
+
+if (!homeDir || !swapDir) throw new Error('SWAP_DIR and HOME_DIR must be specified');
+
+export const token = process.env.BOT_TOKEN as string
+
+export const publishersIds = String(process.env.PUBLISHERS_IDS)
+  .split(',')
+  .map((id: string): number => parseInt(id))
+
+export const adminId = Number(process.env.ADMIN_ID);
