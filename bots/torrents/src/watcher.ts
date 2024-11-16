6237064a1e144b0e454218e6aa6477ce3f9085e1
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-11-16 16:52:50 +0800

    feat: reduce amount of events

--- bots/torrents/src/watcher.ts
+++ bots/torrents/src/watcher.ts
@@ -1,4 +1,5 @@
 import expandTilde from 'expand-tilde';
+import crypto from 'node:crypto';
 import EventEmitter from 'node:events';
 import { watch, WatchEventType, readFile } from 'node:fs';
 import path from 'node:path';
@@ -7,23 +8,49 @@ const trackingFile = 'tracking.json'
 const trackingPath = path.join('~/.config/torrents', trackingFile);
 const fullTrackingPath = expandTilde(trackingPath);
 
+export type TrackingTopic = {
+  type: 'tv_show',
+  subsOnly: boolean,
+  query: string,
+  guid: string,
+};
+export type TrackingConfig = {
+  topics: TrackingTopic[],
+}
+
 export class ConfigWatcher extends EventEmitter {
+  private hashes = new Map<string, string>();
+
   constructor() {
     super()
 
     watch(fullTrackingPath, (event: WatchEventType, filename: string | null) => {
-      console.log('watcher triggered', event, filename);
       if (event === 'change' && filename === trackingFile) {
-        readFile(fullTrackingPath, (err, content) => {
+        readFile(fullTrackingPath, (err, buf) => {
           if (err) {
             console.log('Error while tryint to watch', fullTrackingPath);
             console.error(err);
             return;
           }
           try {
-            const config = JSON.parse(content.toString());
+            const content = buf.toString();
+            const configHash = this.getHash(content);
+            if (this.hashes.get('full') === configHash) {
+              return;
+            }
+            this.hashes.set('full', configHash);
 
-            this.emit('config', config);
+            const config = JSON.parse(content.toString()) as TrackingConfig;
+
+            config.topics.forEach((topic) => {
+              const topicHash = this.getHash(JSON.stringify(topic));
+              if (this.hashes.get(topic.guid) === topicHash) {
+                return;
+              }
+              this.hashes.set(topic.guid, topicHash);
+
+              this.emit('topic', topic);
+            })
           } catch (e) {
             console.log('Failed to parse tracking config');
             console.error(e)
@@ -31,5 +58,13 @@ export class ConfigWatcher extends EventEmitter {
         })
       }
     });
+  }
+
+  getHash(str: string): string {
+    const hash = crypto.createHash('sha256');
+
+    hash.update(str);
+
+    return hash.digest('hex');
   }
 }
