d43c9051089f711f0c5965fdb9a3d59c0733b3e2
Eugeny Dementyev <eugeny.dementev@ya.ru>
2025-03-29 20:14:21 +0800

    feat: reschedule and then emit

--- bots/torrents/src/scheduler.ts
+++ bots/torrents/src/scheduler.ts
@@ -88,11 +88,11 @@ export class Scheduler extends EventEmitter {
         const topic = await this.getTopic(topicConfig.guid);
         if (!topic) {
           this.logger.error(new Error(`Topic is not found: ${guid}`));
-          this.emit('topic', topicConfig);
           const timeout = this.calculateTimeout(topicConfig.type, new Date().toString()) + Math.floor(Math.random() * THIRTY_MINUTES_MS);
           const timestamp = Date.now() + timeout;
           this.logger.info('Scheduling next topic check', { guid, date: new Date(timestamp) });
           this.#plannedCheckTimeMap.set(guid, timestamp);
+          this.emit('topic', topicConfig);
           continue;
         }
 
